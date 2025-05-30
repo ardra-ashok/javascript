9e3158e78f2280e0cc2fca793b13ca4ea65090e9
ardra-ashok <ardraasok@yahoo.in>
2025-05-30 23:21:42 +1000

    removed pynum

--- sample_ques.js
+++ sample_ques.js
@@ -8,4 +8,15 @@ function sumFunc(a, b) {
 
 console.log(sumFunc(2, 3));
 
+// Write a JavaScript program to find the maximum number in an array. 
 
+
+let arr = [78, 20, 40, 50, 89, 70, 60];
+let max = arr[0]
+for (let i = 0; i < arr.length; i++)
+{
+ if (arr[i] > max)
+  max = arr[i];
+}
+
+console.log(max);
